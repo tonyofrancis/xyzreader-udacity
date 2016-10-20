d5e0ae883a1c89ea1ebb00d3223f7bef3f8b2c70
Tonyo Francis <tonyo.f@irokopartners.com>
2016-10-20 16:25:18 -0400

    Update main activity root view
    Change main activity root view to cordinator layout

--- app/src/main/java/com/tonyostudio/xyzreader/ui/ArticleListActivity.java
+++ app/src/main/java/com/tonyostudio/xyzreader/ui/ArticleListActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -32,7 +33,7 @@ import io.realm.RealmResults;
 public class ArticleListActivity extends AppCompatActivity implements ArticleAdapter.OnArticleClickListener {
 
     @BindView(R.id.container)
-    RelativeLayout container;
+    CoordinatorLayout container;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.swipe_refresh_layout)
